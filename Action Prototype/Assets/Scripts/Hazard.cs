6184033bf5aa60505aeffcfed990ccad1d4b0f56
Clare-M <64334337+cmcf@users.noreply.github.com>
2023-10-16 17:15:46 +0100

    Obstacle crush fix
    Added additional collider so that the player dies if they are under the obstacle

--- Action Prototype/Assets/Scripts/Hazard.cs
+++ Action Prototype/Assets/Scripts/Hazard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("Dog") || collision.CompareTag("Bear"))
