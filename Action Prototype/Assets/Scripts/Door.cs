4e82c8b64f58f58f4d5c43d319fdf39da1772c98
Clare-M <64334337+cmcf@users.noreply.github.com>
2023-10-16 17:20:27 +0100

    Removed unused references

--- Action Prototype/Assets/Scripts/Door.cs
+++ Action Prototype/Assets/Scripts/Door.cs
@@ -6,20 +6,17 @@ public class Door : MonoBehaviour
     public bool isOpen;
     Animator animator;
     Lever lever;
-    BoxCollider2D boxCollider;
 
     [System.Obsolete]
     private void Start()
     {
         animator = GetComponent<Animator>();
         lever = FindObjectOfType<Lever>();
-        boxCollider= GetComponent<BoxCollider2D>();
     }
     public void OpenDoor()
     {
         Debug.Log("open");
         animator.SetBool("isOpen", true);
-        
         Invoke("CloseDoor", timer);
     }
     public void CloseDoor()
