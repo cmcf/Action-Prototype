ae086b52f04d9c9c427ac848b4d4447841baa6ea
Clare-M <64334337+cmcf@users.noreply.github.com>
2023-10-20 15:35:43 +0100

    Platform decrease & level 2 setup

---
+++ Action Prototype/Assets/Scripts/PlatformSizeDecrease.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSizeDecrease : MonoBehaviour
+{
+    [SerializeField] float decreaseDelay = 0.1f; // The delay before the platform reduces in size
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Dog"))
+        {
+            StartCoroutine(DecreasePlatform());
+        }
+    }
+
+    private IEnumerator DecreasePlatform()
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(decreaseDelay);
+
+        anim.SetTrigger("SizeDown");
+        Invoke("DestroyPlatform", 0.4f);
+
+    }
+
+    void DestroyPlatform()
+    {
+        gameObject.SetActive(false);
+    }
+}
