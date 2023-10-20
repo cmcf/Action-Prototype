278dc396e7b8a5b694c199486b1d0876f659ed7e
Clare-M <64334337+cmcf@users.noreply.github.com>
2023-10-20 23:29:55 +0100

    Added timer

---
+++ Action Prototype/Assets/Scripts/Timer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+
+    [SerializeField] float totalTime = 50f;
+
+    [SerializeField] float currentTime;
+    [SerializeField] float timeDelay = 0.001f;
+
+    public bool stopTimer = false;
+    void Start()
+    {
+        timerSlider.maxValue = totalTime;
+        timerSlider.value = totalTime;
+        StartTimer();
+    }
+
+    void StartTimer()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        while (!stopTimer)
+        {
+            // Decreases slider time
+            totalTime -= Time.deltaTime;
+            yield return new WaitForSeconds(timeDelay);
+
+            if (totalTime <= 0)
+            {
+                stopTimer = true;
+            }
+            // Updates slider value
+            if (!stopTimer)
+            {
+                timerSlider.value = totalTime;
+            }
+        }
+        Debug.Log("Out of time");
+        
+    }
+
+    void StopTime()
+    {
+        stopTimer = true;
+    }
+}
