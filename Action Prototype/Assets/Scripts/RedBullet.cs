392804c226e4bc45b9f261a789b6fc0ccdf1857c
Clare-M <64334337+cmcf@users.noreply.github.com>
2023-11-12 22:09:30 +0000

    Moving box update
    Improve the smoothness of the box movement and stop jittering when hit by bullet

--- Action Prototype/Assets/Scripts/RedBullet.cs
+++ Action Prototype/Assets/Scripts/RedBullet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RedBullet : MonoBehaviour
 {
-    Enemy enemy;
     [System.Obsolete]
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -12,12 +11,6 @@ public class RedBullet : MonoBehaviour
         if (other.CompareTag("Ignore"))
         {
             return;
-        }
-        if (other.CompareTag("Enemy"))
-        {
-            Debug.Log("Red bullet hit the enemy.");
-            enemy = other.GetComponent<Enemy>();
-            enemy.EnemyDeath();
         }
 
         Destroy(gameObject);
